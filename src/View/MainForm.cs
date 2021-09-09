301a9b7f9c7b2043c4246ef95d967f370a4c6a01
Patrick Tedeschi <patrick@tedeschi.com.br>
2021-09-09 16:33:10 +1200

    Empty Project

---
+++ src/View/MainForm.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace Tedeschi.NFT.net
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
